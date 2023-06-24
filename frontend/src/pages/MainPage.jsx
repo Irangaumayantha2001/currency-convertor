ad0959bc5b2b5aa6499560e6ae79fd3db16a8f69
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-06-24 11:52:09 +0530

    update

---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>Convert your currencies Today</h1>
+    </div>
+  );
+};
+
+export default MainPage;
