54a5c7ac5be9d4f556aee98a3a3d53eb6573cc66
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-06-24 20:36:56 +0530

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
+import axios from "axios";
+
+
 const express =require("express");
 const cors =require("cors");
-const axios =require("axios");
+const axios = require("axios");
+
+
 
 const app =express();
 
